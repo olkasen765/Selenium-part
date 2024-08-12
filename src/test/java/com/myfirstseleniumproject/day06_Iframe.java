3b8eadaeeb1721caadcccee7d39b2f7405429aff
olkasen765 <o.kassenov@eot.kz>
2024-08-12 04:05:16 -0500

    Selenium part

--- src/test/java/com/myfirstseleniumproject/day06_Iframe.java
+++ src/test/java/com/myfirstseleniumproject/day06_Iframe.java
@@ -24,7 +24,9 @@ public class day06_Iframe extends TestBase {
         //Then Assert that "Left List Item 29" is the last element in the "Left"
         List<WebElement> listOfElementsForLeft = driver.findElements(By.xpath("//li"));
 
-        String lastElementOfLeft = listOfElementsForLeft.get(listOfElementsForLeft.size()-1).getText();
+//        String lastElementOfLeft = listOfElementsForLeft.get(listOfElementsForLeft.size()-1).getText();
+        String lastElementOfLeft = driver.findElement(By.xpath("//li[last()]")).getText();
+
 
 //        for(WebElement each : listOfElementsForLeft){
 //
@@ -34,7 +36,27 @@ public class day06_Iframe extends TestBase {
         Assertions.assertTrue(lastElementOfLeft.equals("Left List Item 29"));
 
         System.out.println("lastElementOfLeft = " + lastElementOfLeft);
+
         //And Assert that "Middle List Item 39" is the last element in the "Middle"
+
+        //Driver is on the left iframe so we can not go to middle iframe directly. Thats why go to main level or upper level...
+        driver.switchTo().defaultContent();
+
+        //it is time to go to middle iframe...
+
+        driver.switchTo().frame(driver.findElement(By.xpath("//frame[@name='middle']")));
+
+
+        //And Assert that "Middle List Item 39" is the last element in the "Middle"
+
+
+        String lastElementForMiddle =  driver.findElement(By.xpath("//li[last()]")).getText();
+
+
+        System.out.println(lastElementForMiddle);
+
+        Assertions.assertTrue(lastElementForMiddle.equals("Middle List Item 39"));
+
 
 
     }
